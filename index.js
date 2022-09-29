4629027f197ff132cbb2b2e9d4611e8aff98d5c5
ginniss2022 <gabevaughninn@gmail.com>
2022-09-29 19:14:21 +0000

    9&10 complete

--- index.js
+++ index.js
@@ -59,4 +59,16 @@ function triangleArea(height,base){
     let multiply = height * base;
     return multiply * 0.5;
 }
-console.log(triangleArea(4,5));
+
+function betweenTwentyAndFourty(num){
+    if(num > 20 && num < 40){
+        return true 
+    }else {return false}
+}
+
+function largest(int1, int2, int3){if(int1 > int2 && int1 > int3){
+    console.log(int1)
+}else if(int2 > int1 && int2 > int3){
+    console.log(int2)
+}else console.log(int3)
+}
